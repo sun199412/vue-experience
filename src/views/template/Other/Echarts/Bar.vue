394f976216a47314ae7d1441fd57759d7a6e2bc9
dj49846917 <82104785@qq.com>
2019-08-30 00:19:53 +0800

    echartsDemo

---
+++ src/views/template/Other/Echarts/Bar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>静态数据</div>
+    <v-chart :options="option" />
+  </div>
+</template>
+
+<script>
+import ECharts from 'vue-echarts'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/legend'
+export default {
+  components: {
+    'v-chart': ECharts
+  },
+  data() {
+    return {
+      option: {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar'
+        }]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.echarts{
+	left: -55px;
+	top: -22px;
+	transform: scale(0.9);
+}
+</style>
--- src/views/template/Other/Echarts/index.vue
+++ src/views/template/Other/Echarts/index.vue
@@ -5,10 +5,13 @@
         <span>echarts的使用</span>
       </div>
       <el-row class="progressBox">
-        <el-col :md="12" :xs="24" :sm="24" class="progressBox_start">
+        <el-col :md="11" :xs="24" :sm="24" class="progressBox_start">
           <LineDemo
             :data="data"
           />
+        </el-col>
+        <el-col :offset="1" :md="12" :xs="24" :sm="24" class="progressBox_start">
+          <BarDemo />
         </el-col>
       </el-row>
     </el-card>
@@ -18,9 +21,11 @@
 <script>
 import { getTotal } from '@/api/template/charts/charts'
 import LineDemo from '@/views/template/Other/Echarts/Line'
+import BarDemo from '@/views/template/Other/Echarts/Bar'
 export default {
   components: {
-    LineDemo
+    LineDemo,
+    BarDemo
   },
   data() {
     return {
